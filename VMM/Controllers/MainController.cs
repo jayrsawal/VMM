01637a6d82cbb89e1b9376e880b3e0c9dd24f44d
Victor Sawal <jayrsawal@gmail.com>
2017-01-11 20:59:37 -0500

    Add project files.

---
+++ VMM/Controllers/MainController.cs
@@ -0,0 +1,26 @@
+﻿using VMM.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMM.Controllers {
+    public class MainController : ControllerTemplate {
+        // GET: Main
+        [AllowAnonymous]
+        public ActionResult Index() {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Error() {
+            try {
+                this.LogException(Server.GetLastError());
+            } catch { }
+            return View();
+        }
+    }
+}
---
+++ VMM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VMM.MvcApplication" Language="C#" %>
